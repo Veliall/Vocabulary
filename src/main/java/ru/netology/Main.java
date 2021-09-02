af0223afb66bd58b7cb2192b7315d70ddc9c007a
Igor Khristiuk <ikhristiuk@gmail.com>
2021-09-02 15:51:34 +0300

    initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printVocab(getVocabulary("Якорь Азбука Букварь Слово Едрени-фени Велик Группа Дверь Зверь Труп"));
+
+    }
+
+//    Отличительные черты функционального стиля - ну стримы уже сами по себе на это указывают
+//    плюс "чистота" методов, детерминированость и отсутствие сайд-эффектов
+    public static List<String> getVocabulary(String string) {
+        return Arrays.stream(string.split(" ")).sorted().collect(Collectors.toList());
+    }
+
+    public static void printVocab(List<String> vocab) {
+        vocab.forEach(System.out::println);
+    }
+}
