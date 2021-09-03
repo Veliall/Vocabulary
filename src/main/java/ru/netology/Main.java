0fe25dfb1db234e93a0c2470bc7952e861347a71
Igor Khristiuk <ikhristiuk@gmail.com>
2021-09-03 20:34:19 +0300

    Added ".distinct()"

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -8,14 +8,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        printVocab(getVocabulary("Якорь Азбука Букварь Слово Едрени-фени Велик Группа Дверь Зверь Труп"));
+        printVocab(getVocabulary("Якорь Якорь Якорь Азбука Букварь" +
+                " Слово Едрени-фени Велик Группа Дверь Зверь Труп"));
 
     }
 
-//    Отличительные черты функционального стиля - ну стримы уже сами по себе на это указывают
+    //    Отличительные черты функционального стиля - ну стримы уже сами по себе на это указывают
 //    плюс "чистота" методов, детерминированость и отсутствие сайд-эффектов
     public static List<String> getVocabulary(String string) {
-        return Arrays.stream(string.split(" ")).sorted().collect(Collectors.toList());
+        return Arrays.stream(string.split(" "))
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     public static void printVocab(List<String> vocab) {
